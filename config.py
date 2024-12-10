91243c4f28925de7978b62f7c21c52160b8a44cb
SᴛᴇʟʟᴀʀLᴀʙs <158211667+Daneil-k@users.noreply.github.com>
2024-12-10 17:37:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather, --⚠️ REQUIRED--
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7679609582:AAFySWhzfQgA33PTLoM8HMIl3o0bc1db75I")
 
 #Your API ID from my.telegram.org, --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "22165209"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "f8cfd997b8d868ad6442141be56629a7")
 
 #Your db channel Id --⚠️ REQUIRED--
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001685956766"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7720951169"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
@@ -28,11 +28,11 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
 PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://t4tsa:qJxfq76UNhEaPyEP@cluster0.dbvtt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-DB_NAME = os.environ.get("DATABASE_NAME", "adbotv3Xlight")
+DB_NAME = os.environ.get("DATABASE_NAME", "IrisFileStore510Bot")
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
 PICS = (os.environ.get("PICS", "https://telegra.ph/file/5094c60f1122bbae9b3d9.jpg https://telegra.ph/file/463501fe337f02dc034ba.jpg https://telegra.ph/file/ad3486519fd59f73f7f46.jpg https://telegra.ph/file/8d4867e3d7d8e8db70f73.jpg https://telegra.ph/file/3b8897b58d83a512a56ac.jpg https://telegra.ph/file/11115f9a5c035e2d90bd8.jpg https://telegra.ph/file/a292bc4b99f9a1854f6d7.jpg https://telegra.ph/file/94aac0f8141dc44eadfc6.jpg https://telegra.ph/file/1f8d855fb7a70b4fcaf68.jpg https://telegra.ph/file/849b567f8072117353c5c.jpg https://telegra.ph/file/e8555407480d52ac1a6b7.jpg https://telegra.ph/file/2a301e221bf3c800bb48c.jpg https://telegra.ph/file/faefbf4a710eb05647d9c.jpg https://telegra.ph/file/6219c9d5edbeecfd3a45e.jpg https://telegra.ph/file/db1f952a28b0aa53bedb1.jpg https://telegra.ph/file/32797f53236187e9f5e1f.jpg https://telegra.ph/file/f1038a205b9db5018f1aa.jpg https://telegra.ph/file/88fb9950df687ff6caa58.jpg https://telegra.ph/file/63855c358fdd9a02c717c.jpg https://telegra.ph/file/34fb4b74d70bfc2e9d59c.jpg https://telegra.ph/file/e92c0b6efb0a77b316e04.jpg https://telegra.ph/file/2f3adfb321584ad39fd15.jpg")).split() #Required
