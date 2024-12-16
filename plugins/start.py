32451bf8b5038ff28a0644ff972e905903454409
shidoteshika2 <shidoteshika2@gmail.com>
2024-12-16 22:52:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -104,10 +104,10 @@ async def start_command(client: Client, message: Message):
                         
         #if AUTO_DEL and last_message:
                 #asyncio.create_task(auto_del_notification(client.username, last_message, DEL_TIMER, message.command[1]))
-                
+
         textads = client.textads
-        if len(messages) > 1 and textads:
-            await message.reply(text=textads.html)
+        if (len(messages) > 1) and textads:
+            await message.reply(text=textads.html, disable_web_page_preview=True)
                         
     else:   
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('😊 About Me', callback_data= 'about'), InlineKeyboardButton('🔒Close', callback_data='close')]])
